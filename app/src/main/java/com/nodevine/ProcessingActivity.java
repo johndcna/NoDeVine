c1b9f41f47b3b7a952a4f16e28bbc8d174005065
johndcna <decenajac@gmail.com>
2015-12-30 08:48:06 +0800

    Added temporary ProcessingActivity

--- app/src/main/java/com/nodevine/ProcessingActivity.java
+++ app/src/main/java/com/nodevine/ProcessingActivity.java
@@ -1,30 +1,26 @@
 package com.nodevine;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 
 
-public class MainActivity extends Activity {
+public class ProcessingActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Remove title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_processing);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_processing, menu);
         return true;
     }
 
@@ -41,10 +37,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void goBuild(View view) {
-        Intent intent = new Intent(this,ProcessingActivity.class);
-        startActivity(intent);
     }
 }
