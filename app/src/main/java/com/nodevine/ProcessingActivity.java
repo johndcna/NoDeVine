53388d952390c64c8acd4069eeb29991afd00c99
johndcna <decenajac@gmail.com>
2016-01-09 15:04:24 +0800

    Added timer

--- app/src/main/java/com/nodevine/ProcessingActivity.java
+++ app/src/main/java/com/nodevine/ProcessingActivity.java
@@ -2,18 +2,38 @@ package com.nodevine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
 
 public class ProcessingActivity extends Activity {
+    TextView textGoesHere;
+    long startTime;
+    long countUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_processing);
+
+        Chronometer stopWatch = (Chronometer) findViewById(R.id.chronometer);
+        startTime = SystemClock.elapsedRealtime();
+
+        textGoesHere = (TextView) findViewById(R.id.editText);
+        stopWatch.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){
+            @Override
+            public void onChronometerTick(Chronometer arg0) {
+                countUp = (SystemClock.elapsedRealtime() - arg0.getBase()) / 1000;
+                String asText = (countUp / 60) + ":" + (countUp % 60);
+                textGoesHere.setText(asText);
+            }
+        });
+        stopWatch.start();
     }
 
 
@@ -38,4 +58,6 @@ public class ProcessingActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
